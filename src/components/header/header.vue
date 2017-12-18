82dd1f65730b09151b4ceaf57d0141f8c71a54e5
henley02 <410451447@qq.com>
2017-12-18 20:41:15 +0800

    [+]添加icon

---
+++ src/components/header/header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div></div>
+</template>
+<script type="text/ecmascript-6">
+    export default {
+        name: '',
+        data() {
+            return {}
+        },
+        methods: {},
+        created() {
+        },
+        mounted() {
+        },
+    }
+</script>
+<style scoped>
+</style>
