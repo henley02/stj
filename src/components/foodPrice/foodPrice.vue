417a8de05a26eabe259b538c475024e9d5e15154
henley02 <410451447@qq.com>
2018-04-25 18:19:39 +0800

    [+]add component footDetail and split

---
+++ src/components/foodPrice/foodPrice.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="price">
+    <span class="now">¥{{food.price}}</span><span v-show="food.oldPrice"
+                                                  class="old">¥{{food.oldPrice}}</span>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    name: 'FoodPrice',
+    props: {
+      food: {type: Object}
+    }
+  };
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .price
+    font-weight: 700
+    line-height 24px
+    .now
+      margin-right: 18px
+      font-size: 14px
+      color: rgb(240, 20, 20)
+    .old
+      text-decoration: line-through
+      font-size: 10px
+      color: rgb(147, 153, 159)
+</style>
---
+++ src/components/footDetail/footDetail.vue
@@ -0,0 +1,172 @@
+<template>
+  <transition name="move">
+    <div v-show="visible" class="food" ref="food">
+      <div class="food-content">
+        <div class="image-header">
+          <img :src="food.image"/>
+          <div class="back" @click="close()">
+            <i class="icon-arrow_lift"></i>
+          </div>
+        </div>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}%</span>
+          </div>
+          <food-price :food="food"></food-price>
+
+          <div class="cartcontrol-wrapper" v-if="food.count&& food.count>0">
+            <cart-control :food="food" @add="addFood"></cart-control>
+          </div>
+          <div class="buy" v-if="!food.count || food.count===0" @click.stop.prevent="addFirst($event)">加入购物车</div>
+        </div>
+        <template v-if="food.info">
+          <split></split>
+          <div class="info">
+            <h1 class="title">商品信息</h1>
+            <p class="text">{{food.info}}</p>
+          </div>
+        </template>
+      </div>
+    </div>
+  </transition>
+</template>
+<script type="text/ecmascript-6">
+  /**
+   * 商品详情
+   */
+  import foodPrice from './../foodPrice/foodPrice.vue';
+  import cartControl from './../cartControl/cartControl.vue';
+  import split from './../split/split.vue';
+
+  import BScroll from 'better-scroll';
+
+  export default {
+    name: 'FootDetail',
+    props: {
+      food: {type: Object},
+      visible: {type: Boolean}
+    },
+    data() {
+      return {
+        showFlag: false,
+        scroll: null
+      };
+    },
+    methods: {
+      addFood(target) {
+        this.$emit('add', target);
+      },
+      addFirst(event) {
+        this.$emit('add', event.target);
+        if (!this.food.count) {
+          this.$set(this.food, 'count', 1);
+        }
+      },
+      close() {
+        this.$emit('update:visible', false);
+      }
+    },
+    created() {
+      this.$nextTick(() => {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.food, {
+            click: true
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      });
+    },
+    components: {
+      foodPrice,
+      cartControl,
+      split
+    }
+  };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  .food
+    position: fixed
+    top: 0
+    left: 0
+    bottom: 48px
+    z-index: 30
+    width: 100%
+    background: #fff
+    transform: translate3d(0, 0, 0)
+    &.move-enter-active, &.move-leave-active
+      transition: all 0.2s linear
+    &.move-enter, &.move-leave-active
+      transform: translate3d(100%, 0, 0)
+    .image-header
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 100%
+      img
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+      .back
+        position: absolute
+        top: 10px
+        left: 0
+        .icon-arrow_lift
+          display: block
+          padding: 10px
+          font-size: 20px
+          color: #ffffff
+
+    .content
+      position: relative
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        height: 10px
+        line-height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .sell-count
+          margin-right: 12px
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #ffffff
+        background: rgb(0, 160, 220)
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7, 17, 27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77, 85, 93)
+</style>
