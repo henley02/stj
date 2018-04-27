db8bae680f5a9c2c5f51b842c576b2e8ce3182d0
henley02 <410451447@qq.com>
2018-04-27 18:22:33 +0800

    [+]add page seller

--- src/pages/seller/seller.vue
+++ src/pages/seller/seller.vue
@@ -1,15 +1,55 @@
 <template>
+  <div class="seller">
+    <div class="seller-content">
+      <div class="overview">
+        <h1 class="title">{{seller.name}}</h1>
+        <div class="desc">
+          <star :size="36" :score="seller.score"></star>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
+        </div>
+        <ul class="remark">
+          <li class="block">
+            <h2>起送价</h2>
+            <div class="content">
+              <span class="stress">{{seller.minPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>商家配送</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryPrice}}</span>元
+            </div>
+          </li>
+          <li class="block">
+            <h2>平均配送时间</h2>
+            <div class="content">
+              <span class="stress">{{seller.deliveryTime}}</span>分钟
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 <script type="text/ecmascript-6">
+  import star from 'components/star/star.vue';
+
   export default {
     name: 'seller',
-    data () {
+    components: {
+      star
+    },
+    props: {
+      seller: {type: Object}
+    },
+    data() {
       return {};
     },
     methods: {},
-    created () {
+    created() {
     },
-    mounted () {
+    mounted() {
     }
   };
 </script>
